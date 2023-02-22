5dc2f5e7752db0f26dc846810e65a50de62cbe77
hmhashem <hmhashem@LPCAIQ1087.ejadacairo.local>
2023-02-22 22:31:36 +0300

    Adding testng

--- 1/src/test/java/Test1.java
+++ 1/src/test/java/Test1.java
@@ -1,11 +1,13 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Test1 {
 
 	public static void main(String[] args) {
-		String projectpath=System.getProperty("user.dir");
-		System.setProperty("webdriver.chrome.driver", projectpath+"/drivers/chromedriver.exe");
+		//String projectpath=System.getProperty("user.dir");
+		//System.setProperty("webdriver.chrome.driver", projectpath+"/drivers/chromedriver.exe");
+		//WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver ();
 		driver.get("https://masrawy.com");
 			
